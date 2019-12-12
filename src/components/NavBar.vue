a2124e25f6d59515da0f15af6c63a57593ede0e1
isBoom <1520285660@qq.com>
2019-12-12 21:12:56 +0800

    1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <el-menu mode="horizontal" router>
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/postVideo">投稿</el-menu-item>
+    <el-menu-item index="/about">关于我们</el-menu-item>
+
+  </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+</style>
---
+++ src/views/PostVideo.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="postVideo">
+<h3>视频投稿:</h3>
+  <el-form ref="form" :model="form" label-width="100px">
+
+    <el-form-item label="标题">
+      <el-input v-model="form.title"></el-input>
+    </el-form-item>
+
+    <el-form-item label="视频描述">
+      <el-input type="textarea" v-model="form.ingo" size="medium"></el-input>
+    </el-form-item>
+
+    <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+    </el-form-item>
+
+  </el-form>
+</div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
+</script>
